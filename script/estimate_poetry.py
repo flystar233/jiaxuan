95e14b47150935b1923cf6e8a8d23f6aecb684a2
Xfly <18374858141@163.com>
2024-04-02 09:29:05 +0800

    Update estimate_poetry.py

--- script/estimate_poetry.py
+++ script/estimate_poetry.py
@@ -15,13 +15,13 @@ def estimate_poetry(text):
     text_drop = re.sub("[^\u4e00-\u9fa5]", "", text)
     length = len(text_drop)
     split_length = [len(i) for i in re.split('，|。|、',text) if i]
-    guess_cipai='未查到词牌名'
-    pingze_database = []
+    guess_cipai=''
+    pingze_database = ''
     for index, cipai in df.iterrows():
         if length == cipai['总数'] and str(split_length) == cipai['分段字数']:
             guess_cipai = cipai['词牌名']
             pingze_database = cipai['韵律']
-
+    
     pingze_database=  list(re.sub("[^\u4e00-\u9fa5]", "", pingze_database))
 
     pingze_text = []
@@ -40,12 +40,15 @@ def estimate_poetry(text):
 
     score = 0
     issue_data = []
-    for item1, item2 in zip(pingze_text,pingze_database):
-        if item1[1]!=item2 and item2!='中':
-            issue_data.append((item1, item2))
-        elif item1[1]==item2 or item2=='中':
-            score+=1
-    score= score/length*100
+    if guess_cipai:
+        for item1, item2 in zip(pingze_text,pingze_database):
+            if item1[1]!=item2 and item2!='中':
+                issue_data.append((item1, item2))
+            elif item1[1]==item2 or item2=='中':
+                score+=1
+        score= score/length*100
+    else:
+        pass
     return guess_cipai,score,issue_data
 
 text = "云气上林梢，毕竟非空非色。风景不随人去，到而今留得。老无情味到篇章，诗债怕人索。却笑近来林下，有许多词客。"
